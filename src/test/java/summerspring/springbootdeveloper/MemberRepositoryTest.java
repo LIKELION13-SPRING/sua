9346b34ddfa03449106d1468bc358367c6f8202a
Sua Jun <mjun3712@gmail.com>
2025-07-18 16:07:45 +0900

    Ch5.

--- src/test/java/summerspring/springbootdeveloper/MemberRepositoryTest.java
+++ src/test/java/summerspring/springbootdeveloper/MemberRepositoryTest.java
@@ -90,4 +90,16 @@ class MemberRepositoryTest {
         assertThat(memberRepository.findAll().size()).isZero();
     }
 
+    @Sql("/insert-members.sql")
+    @Test
+    void update() {
+        //given
+        Member member = memberRepository.findById(2L).get();
+
+        //when
+        member.changeName("BC");
+
+        //then
+        assertThat(memberRepository.findById(2L).get().getName()).isEqualTo("BC");
+    }
 }
