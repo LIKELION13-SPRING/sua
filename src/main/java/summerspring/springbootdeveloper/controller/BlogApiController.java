b441f59d60366726c49dd32be49b1d3ecc9c1d4e
Sua Jun <mjun3712@gmail.com>
2025-07-19 17:49:07 +0900

    Ch6. 블로그 글 조회

--- src/main/java/summerspring/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/summerspring/springbootdeveloper/controller/BlogApiController.java
@@ -3,12 +3,14 @@ package summerspring.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import summerspring.springbootdeveloper.Getmapping;
 import summerspring.springbootdeveloper.domain.Article;
 import summerspring.springbootdeveloper.dto.AddArticleRequest;
+import summerspring.springbootdeveloper.dto.ArticleResponse;
 import summerspring.springbootdeveloper.service.BlogService;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,5 +24,23 @@ public class BlogApiController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok().body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
     }
 }
--- src/main/java/summerspring/springbootdeveloper/service/BlogService.java
+++ src/main/java/summerspring/springbootdeveloper/service/BlogService.java
@@ -6,6 +6,8 @@ import summerspring.springbootdeveloper.domain.Article;
 import summerspring.springbootdeveloper.dto.AddArticleRequest;
 import summerspring.springbootdeveloper.repository.BlogRepository;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class BlogService {
@@ -14,5 +16,14 @@ public class BlogService {
 
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    public Article findById(Long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
     }
 }
