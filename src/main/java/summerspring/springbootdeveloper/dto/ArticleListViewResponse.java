4c86d908600e9f8ca32abd6817ee67cde4e162d0
Sua Jun <mjun3712@gmail.com>
2025-07-21 19:54:14 +0900

    Ch7. 블로그 글 뷰 구ã현 완료

---
+++ src/main/java/summerspring/springbootdeveloper/dto/ArticleListViewResponse.java
@@ -0,0 +1,18 @@
+package summerspring.springbootdeveloper.dto;
+
+import lombok.Getter;
+import summerspring.springbootdeveloper.domain.Article;
+
+@Getter
+public class ArticleListViewResponse {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
