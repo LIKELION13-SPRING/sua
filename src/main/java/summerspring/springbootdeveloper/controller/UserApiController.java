4d69e9209394918465de6680669a5a7d59a12981
Sua Jun <mjun3712@gmail.com>
2025-07-23 19:52:10 +0900

    Ch8. login logout signup

--- src/main/java/summerspring/springbootdeveloper/controller/UserApiController.java
+++ src/main/java/summerspring/springbootdeveloper/controller/UserApiController.java
@@ -1,7 +1,12 @@
 package summerspring.springbootdeveloper.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import summerspring.springbootdeveloper.dto.AddUserRequest;
 import summerspring.springbootdeveloper.service.UserService;
@@ -14,6 +19,12 @@ public class UserApiController {
     @PostMapping("/user")
     public String signup(AddUserRequest request) {
         userService.save(request);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
         return "redirect:/login";
     }
 }
